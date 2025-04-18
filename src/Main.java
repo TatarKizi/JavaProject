692665e0556da2f9a4e12d1fdd09beb01bbe0b0f
Barlas Zukhra <zukhrarinatovna@gmail.com>
2025-04-17 22:04:35 -0400

    If else

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+         if (1 > 2){
+             System.out.println("False");
+         }
     }
 }
 
