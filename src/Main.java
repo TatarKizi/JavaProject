91b0528c8f5e09d012171a8281e9334d1ce26440
Barlas Zukhra <zukhrarinatovna@gmail.com>
2025-04-17 22:57:44 -0400

    Final cgange for Feature 2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
          if (1 > 2){
              System.out.println("False");
          }
